5d7b49fc245b9a7590dfea4359a59e9ec30421c4
cdceballor <35697740+cdceballor@users.noreply.github.com>
2025-03-05 17:43:43 -0500

    Add files via upload

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,33 @@
-export default function page() {
-  return <div>Dashboard Page</div>;
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import { lusitana } from "@/app/ui/fonts";
+import { Suspense } from "react";
+import CardWrapper from "../ui/dashboard/cards";
+import LatestInvoices from "../ui/dashboard/latest-invoices";
+import {
+  CardsSkeleton,
+  LatestInvoicesSkeleton,
+  RevenueChartSkeleton,
+} from "../ui/skeletons";
+
+export default async function Page() {
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <Suspense fallback={<CardsSkeleton />}>
+        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+          <CardWrapper />
+        </div>
+      </Suspense>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
+      </div>
+    </main>
+  );
 }
